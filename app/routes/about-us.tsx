fec9a6e9285a92f6668b2e745de5d44ac1e6d411
Dmitry Honcharuk <dmitry.honcharuk@gmail.com>
2022-04-11 21:04:13 +0300

    removed extra code added tailwind

--- app/routes/about-us.tsx
+++ app/routes/about-us.tsx
@@ -3,12 +3,10 @@ import { Link } from '@remix-run/react';
 export default function Index() {
   return (
     <div>
-      <h1 className="text-3xl pt-36 font-bold text-center">
-        Welcome to Artwork
-      </h1>
+      <h1 className="text-3xl pt-36 font-bold text-center">About Artwork</h1>
       <div className="text-center">
-        <Link to="about-us" className="underline">
-          about us
+        <Link to="/" className="underline">
+          home
         </Link>
       </div>
     </div>
